5c4b471ac317aabf2f976a3797086f7215f36191
fpagnoux <fpagnoux@octo.com>
2015-08-29 01:25:35 +0200

    Build scrapeRCP function

--- private/scraper.js
+++ private/scraper.js
@@ -1,11 +1,31 @@
-var PythonShell = require('python-shell');
+var PythonShell = require('python-shell'),
+	_ = require('lodash');
 
-var options = {
-	scriptPath: 'realclearpolitics-scraper',
-	args: ['http://www.realclearpolitics.com/epolls/2016/president/nv/nevada_democratic_presidential_caucus-5337.html']
+var baseUrl = 'http://www.realclearpolitics.com/epolls/2016/president/',
+	scriptPath = 'realclearpolitics-scraper'
+
+var paths = {
+	nv: 'nv/nevada_democratic_presidential_caucus-5337.html',
+	us: 'us/2016_democratic_presidential_nomination-3824.html'
 }
 
-PythonShell.run('scraper.py', options, function(err, results) {
-	if (err) throw err;
-	console.log(results);
-});
+function scrapeRCP() {
+	_.forEach(paths, function(statePath) {
+		scrapeState(statePath)
+	});
+}
+
+function scrapeState(path) {
+	var url = baseUrl + path;
+	var options = {
+		scriptPath: scriptPath,
+		args: [url]
+	};
+
+	PythonShell.run('scraper.py', options, function(err, results) {
+		if (err) throw err;
+		console.log(results);
+	});
+}
+
+scrapeRCP()
