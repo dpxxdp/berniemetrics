ebf432003172cc2fba424feb9f8cefb8f12b7f5d
fpagnoux <fpagnoux@octo.com>
2015-08-28 22:49:17 +0200

    python-shell test

---
+++ private/scraper.js
@@ -0,0 +1,14 @@
+var PythonShell = require('python-shell');
+
+var options = {
+	scriptPath: 'realclearpolitics-scraper',
+	args: [
+		'http://www.realclearpolitics.com/epolls/2016/president/nv/nevada_democratic_presidential_caucus-5337.html',
+		'test.json'
+	]
+}
+
+PythonShell.run('scraper.py', options, function(err, results) {
+	if (err) throw err;
+	console.log(results);
+});
