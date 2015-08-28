a0c1d78c271a01f6e531d93b8dcd505994b335e1
fpagnoux <fpagnoux@octo.com>
2015-08-28 23:45:20 +0200

    Update python script arguments in node script

--- private/scraper.js
+++ private/scraper.js
@@ -2,10 +2,7 @@ var PythonShell = require('python-shell');
 
 var options = {
 	scriptPath: 'realclearpolitics-scraper',
-	args: [
-		'http://www.realclearpolitics.com/epolls/2016/president/nv/nevada_democratic_presidential_caucus-5337.html',
-		'test.json'
-	]
+	args: ['http://www.realclearpolitics.com/epolls/2016/president/nv/nevada_democratic_presidential_caucus-5337.html']
 }
 
 PythonShell.run('scraper.py', options, function(err, results) {
