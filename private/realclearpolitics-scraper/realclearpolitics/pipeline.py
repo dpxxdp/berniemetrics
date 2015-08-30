759d56878637d9cc9ad8b443f715d0397750e7e4
fpagnoux <fpagnoux@octo.com>
2015-08-30 21:54:12 +0200

    Split date field into startDate & endDate

--- private/realclearpolitics-scraper/realclearpolitics/pipeline.py
+++ private/realclearpolitics-scraper/realclearpolitics/pipeline.py
@@ -4,6 +4,11 @@ import json
 class PollPipeline(object):
 
 	def process_item(self, item, spider):
+		dateString = item['Date']
+		[startDate, endDate] = map(lambda s: s.strip(), dateString.split('-'))
+		item['startDate'] = startDate
+		item['endDate'] = endDate
+		del item['Date']
 		line = json.dumps(dict(item))
 		print(line)
 		return item
